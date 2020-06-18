3afee4aab973eb2931a29b340cecc5e4435fcb4c
FlomoN <flo.giger@gmail.com>
2020-06-19 00:39:04 +0200

    Setup mqtthelp and main

--- espota/src/main.cpp
+++ espota/src/main.cpp
@@ -1,9 +1,30 @@
 #include <Arduino.h>
+#include <mqtthelp.h>
 
-void setup() {
-  // put your setup code here, to run once:
+const char *ssid = "xxxxx";              // The SSID (name) of the Wi-Fi network you want to connect to
+const char *password = "xxxxx";          // Password for your WiFi connection
+const char *mqtt_server = "192.168.0.5"; // Address of your MQTT Broker
+const char *clientName = "testbench";    // Name of this device for registering to the broker
+const char *otaProvider = "";            // Hostname or IP + Port of your OTA Provider
+
+void callback(char *topic, byte *payload, unsigned int length)
+{
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++)
+  {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void setup()
+{
+  MQTTHelp::setup(ssid, password, mqtt_server, clientName, otaProvider, callback);
+}
+
+void loop()
+{
+  MQTTHelp::loop();
 }
---
+++ espota/src/mqtthelp.h
@@ -0,0 +1,128 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+namespace MQTTHelp
+{
+
+  const char *ssid;
+  const char *password;
+  const char *mqtt_server;
+  const char *clientName;
+  const char *otaProvider;
+  std::function<void(char *, uint8_t *, unsigned int)> callback;
+
+  int timer = 0;
+
+  WiFiClient espClient;
+  PubSubClient client(espClient);
+
+  /**
+   * Sets up WiFi connection
+   **/
+  void connectWifi()
+  {
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password); // Connect to the network
+    Serial.print("Connecting to ");
+    Serial.print(ssid);
+    Serial.println(" ...");
+
+    while (WiFi.status() != WL_CONNECTED)
+    { // Wait for the Wi-Fi to connect
+      delay(1000);
+      Serial.print(".");
+    }
+
+    Serial.println('\n');
+    Serial.println("Connection established!");
+    Serial.print("IP address:\t");
+    Serial.println(WiFi.localIP());
+  }
+
+  /**
+   * Handles OTA signals and then calls external callback
+   **/
+  void otacheck(char *topic, byte *payload, unsigned int length)
+  {
+    if (strcmp(topic, (std::string("update/") + std::string(clientName)).c_str()) == 0)
+    {
+      Serial.println("Receiving update...");
+    }
+    callback(topic, payload, length);
+  }
+
+  /**
+   * Handles connection loss to the mqtt Broker and resubscribes to channels
+   * TODO: Give in more channels to resubscribe to
+   **/
+  void reconnect()
+  {
+    while (!client.connected())
+    {
+      Serial.print("Attempting MQTT connection...");
+      // Attempt to connect
+      if (client.connect(clientName))
+      {
+        Serial.println("connected");
+        // Once connected, publish an announcement...
+        // client.publish("outTopic", "hello world");
+        // ... and resubscribe
+        // client.subscribe("inTopic");
+        client.subscribe((std::string("update/") + std::string(clientName)).c_str());
+      }
+      else
+      {
+        Serial.print("failed, rc=");
+        Serial.print(client.state());
+        Serial.println(" try again in 5 seconds");
+        // Wait 5 seconds before retrying
+        delay(5000);
+      }
+    }
+  }
+
+  /**
+   * send out periodical ping with own client Name every 10 seconds
+   **/
+  void ping()
+  {
+    if (millis() - timer > 10000)
+    {
+      timer = millis();
+      client.publish("ping", clientName);
+    }
+  }
+
+  /**
+   * Starting Point 
+   **/
+  void setup(const char *_ssid, const char *_password, const char *_mqtt_server, const char *_clientName, const char *_otaProvider, std::function<void(char *, uint8_t *, unsigned int)> _callback)
+  {
+    ssid = _ssid;
+    password = _password;
+    mqtt_server = _mqtt_server;
+    clientName = _clientName;
+    callback = _callback;
+    otaProvider = _otaProvider;
+
+    Serial.begin(9600); // Start the Serial communication to send messages to the computer
+    delay(10);
+    Serial.println('\n');
+    connectWifi();
+    client.setServer(mqtt_server, 1883);
+    client.setCallback(otacheck);
+    reconnect();
+  }
+
+  void loop()
+  {
+    if (!client.connected())
+    {
+      reconnect();
+    }
+    client.loop();
+    ping();
+  }
+
+} // namespace MQTTHelp
