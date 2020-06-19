2ea8dea53aa4dde461224f9a616ca61e054d5872
FlomoN <flo.giger@gmail.com>
2020-06-19 09:23:30 +0200

    credentials

--- espota/src/main.cpp
+++ espota/src/main.cpp
@@ -1,11 +1,11 @@
 #include <Arduino.h>
 #include <mqtthelp.h>
 
-const char *ssid = "xxxxx";              // The SSID (name) of the Wi-Fi network you want to connect to
-const char *password = "xxxxx";          // Password for your WiFi connection
-const char *mqtt_server = "192.168.0.5"; // Address of your MQTT Broker
-const char *clientName = "testbench";    // Name of this device for registering to the broker
-const char *otaProvider = "";            // Hostname or IP + Port of your OTA Provider
+const char *ssid = "UPC6769240";           // The SSID (name) of the Wi-Fi network you want to connect to
+const char *password = "65Xezmxtjwye";     // Password for your WiFi connection
+const char *mqtt_server = "192.168.0.5";   // Address of your MQTT Broker
+const char *clientName = "testbench";      // Name of this device for registering to the broker
+const char *otaProvider = "ota.h.floi.ml"; // Hostname or IP + Port of your OTA Provider
 
 void callback(char *topic, byte *payload, unsigned int length)
 {
